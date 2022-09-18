9ab18e5ec0a738575339f0b4f8ff03df8837d349
Renan Pereira <renan.souza.dev@gmail.com>
2022-09-18 00:32:14 -0300

    feat: add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,7 +7,23 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      gray: {
+        0: '#fff',
+        100: '#fafafa',
+        200: '#eaeaea',
+        300: '#999999',
+        400: '#888888',
+        500: '#666666',
+        600: '#444444',
+        700: '#333333',
+        800: '#222222',
+        900: '#111111'
+      }
+    },
+    fontFamily: {
+      sans: ['Poppins', ...fontFamily.sans]
+    },
   },
   plugins: [],
 }
