23edaad1bce4e48cde76fb0614a30856e5059d7f
Renan Pereira <renan.souza.dev@gmail.com>
2022-09-24 15:26:13 -0300

    feat(home): create my projects section

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,8 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com" rel="preconnect" />
+        <link href="https://fonts.gstatic.com" rel="preconnect" />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
           rel="stylesheet"
