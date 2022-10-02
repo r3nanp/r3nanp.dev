95ad3f1bd90b44a047b8babab2dfc3abd1c811d4
Renan Pereira <renan.souza.dev@gmail.com>
2022-10-02 01:06:48 -0300

    feat(next-seo): create next-seo config

---
+++ next-seo.config.ts
@@ -0,0 +1,14 @@
+import { NextSeoProps } from 'next-seo';
+
+const seo: NextSeoProps = {
+  title: 'Renan Pereira - Developer',
+  description: 'Software engineer, Javascript enthusiast.',
+  canonical: 'https://r3nanp.io',
+  openGraph: {
+    url: 'https://r3nanp.io',
+    title: 'Renan Pereira - Developer',
+    description: 'Software engineer, Javascript enthusiast.',
+  },
+};
+
+export default seo;
