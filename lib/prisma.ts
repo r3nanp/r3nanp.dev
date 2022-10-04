f7e095305f0f518a7c9914e756358468d24b2fff
Renan Pereira <renan.souza.dev@gmail.com>
2022-10-04 18:02:56 -0300

    feat(prisma): add prisma client

---
+++ lib/prisma.ts
@@ -0,0 +1,19 @@
+/* eslint-disable vars-on-top */
+/* eslint-disable no-var */
+import { PrismaClient } from '@prisma/client';
+
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more:
+// https://pris.ly/d/help/next-js-best-practices
+
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-unused-vars
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
