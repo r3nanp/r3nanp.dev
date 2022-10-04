59b2593e345260beaa3bfa80ca15a48bbfad2ad3
Renan Pereira <renan.souza.dev@gmail.com>
2022-10-04 00:05:55 -0300

    feat(footer): add `NowPlaying` component into Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 
+import NowPlaying from './NowPlaying';
+
 const LINKS = [
   {
     href: '/',
@@ -18,6 +20,8 @@ const LINKS = [
 export function Footer() {
   return (
     <footer className="mx-auto mb-8 flex w-full max-w-2xl flex-col items-start justify-center">
+      <hr className="border-1 mb-8 w-full border-gray-800" />
+      <NowPlaying />
       <div className="grid w-full max-w-2xl grid-cols-1 gap-4 pb-16 sm:grid-cols-3">
         <div className="flex flex-col space-y-4">
           {LINKS.map((link, idx) => (
