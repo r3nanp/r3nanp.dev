0f7fd8a4508c9ed0172800865a7cb870c94c29e2
Renan Pereira <renan.souza.dev@gmail.com>
2022-10-05 19:45:46 -0300

    feat(kbar): add `KbarRoot` context in all app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,17 +4,19 @@ import type { AppProps } from 'next/app';
 
 import { Footer } from 'components/Footer';
 import { Header } from 'components/Header';
+import * as Kbar from 'components/Kbar';
 import seo from 'next-seo.config';
 import { trpc } from 'utils/trpc';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Kbar.Root>
       <NextSeo {...seo} />
       <Header />
+      <Kbar.Content />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </Kbar.Root>
   );
 }
 
