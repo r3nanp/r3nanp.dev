204951a5f3517c340522684c1e74fb79eb53aed8
Renan Pereira <renan.souza.dev@gmail.com>
2022-10-05 10:54:30 -0300

    refactor(blog): change blog-post query

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -13,7 +13,7 @@ export const BlogPost: FC<{ slug: string; title: string; description: string }> 
           <div className="flex flex-col justify-between md:flex-row">
             <h4 className="mb-2 w-full text-lg font-medium text-gray-900 dark:text-gray-100 md:text-xl">{title}</h4>
             <p className="mb-4 w-32 text-left text-gray-500 md:mb-0 md:text-right">
-              {`${data?.count ? data?.count.toLocaleString() : '–––'} views`}
+              {`${data?.view ? data?.view.toLocaleString() : '–––'} views`}
             </p>
           </div>
           <p className="text-gray-600 dark:text-gray-400">{description}</p>
