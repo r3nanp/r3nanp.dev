4064f449bcc5334581a169c1be1c07057ef0a3e2
Renan Pereira <renan.souza.dev@gmail.com>
2022-10-05 22:22:38 -0300

    feat(guestbook): create guestbook router with all possible methods

---
+++ server/router/subroutes/guestbook.ts
@@ -0,0 +1,35 @@
+import { z } from 'zod';
+
+import { t } from '../trpc';
+
+export const guestbookRouter = t.router({
+  getAll: t.procedure.query(async ({ ctx }) => {
+    const messages = await ctx.prisma.guestbook.findMany({
+      select: {
+        name: true,
+        message: true,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
+    return messages;
+  }),
+
+  postMessage: t.procedure.input(
+    z.object({
+      name: z.string(),
+      message: z.string().max(100),
+    }),
+  ).mutation(async ({ ctx, input }) => {
+    const { message, name } = input;
+
+    await ctx.prisma.guestbook.create({
+      data: {
+        message,
+        name,
+      },
+    });
+  }),
+});
