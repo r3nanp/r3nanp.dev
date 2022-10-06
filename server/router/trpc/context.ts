3b5581bd402a9d56e19efa93b91ebdb92a2d3180
Renan Pereira <renan.souza.dev@gmail.com>
2022-10-05 22:30:35 -0300

    feat(trpc): add next-auth `session` in trpc context

--- server/router/trpc/context.ts
+++ server/router/trpc/context.ts
@@ -1,7 +1,25 @@
 import * as trpc from '@trpc/server';
+import { CreateNextContextOptions } from '@trpc/server/adapters/next';
+import { Session, unstable_getServerSession as getServerSession } from 'next-auth';
 
 import { prisma } from 'lib/prisma';
+import { options } from 'pages/api/auth/[...nextauth]';
 
-export const createContext = async () => ({ prisma });
+type CreateContextOptions = {
+  session: Session | null;
+};
+
+const context = async (opts: CreateContextOptions) => ({
+  session: opts.session,
+  prisma,
+});
+
+export const createContext = async (opts: CreateNextContextOptions) => {
+  const { req, res } = opts;
+
+  const session = await getServerSession(req, res, options);
+
+  return context({ session });
+};
 
 export type Context = trpc.inferAsyncReturnType<typeof createContext>;
