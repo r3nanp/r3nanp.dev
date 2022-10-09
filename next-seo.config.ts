e5e7ee261ecc5758b1181b01d3e238beae414d37
Renan Pereira <renan.souza.dev@gmail.com>
2022-10-09 01:33:28 -0300

    refactor(seo): create `DOMAIN_URL` constant

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,11 +1,13 @@
 import { NextSeoProps } from 'next-seo';
 
+import { DOMAIN_URL } from 'constants/variables';
+
 const seo: NextSeoProps = {
   title: 'Renan Pereira - Developer',
   description: 'Software engineer, Javascript enthusiast.',
-  canonical: 'https://r3nanp.io',
+  canonical: DOMAIN_URL,
   openGraph: {
-    url: 'https://r3nanp.io',
+    url: DOMAIN_URL,
     title: 'Renan Pereira - Developer',
     description: 'Software engineer, Javascript enthusiast.',
   },
