70acc891638aba0f7b0eee101aadb5785ba4009b
Renan Pereira <renan.souza.dev@gmail.com>
2022-10-09 00:39:05 -0300

    Merge branch 'main' into feat/og

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
+if (!process.env.NEXT_AUTH_SECRET) throw new Error('Please set NEXTAUTH_SECRET');
+
+if (!process.env.NEXTAUTH_URL) throw new Error('Please set NEXTAUTH_URL');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["avatars.githubusercontent.com"],
+    domains: ['avatars.githubusercontent.com'],
   },
 };
 
