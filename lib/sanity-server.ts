6f730afea185c75dcefadd8dd98ce9c83e797a50
Renan Pereira <renan.souza.dev@gmail.com>
2022-10-13 11:40:17 -0300

    feat(sanity): create `urlForImage` helper

--- lib/sanity-server.ts
+++ lib/sanity-server.ts
@@ -1,6 +1,9 @@
+import createImageBuilder from '@sanity/image-url';
 import { createClient } from 'next-sanity';
 
 import { sanityConfig } from './sanity-config';
+
+const imageBuilder = createImageBuilder(sanityConfig);
 
 export const sanityClient = createClient(sanityConfig);
 
@@ -11,3 +14,5 @@ export const previewClient = createClient({
 });
 
 export const getClient = (preview: boolean) => (preview ? previewClient : sanityClient);
+
+export const urlForImage = (source: object) => imageBuilder.image(source).auto('format').fit('max');
